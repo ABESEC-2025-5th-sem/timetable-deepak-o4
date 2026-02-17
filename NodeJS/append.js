b268f57612acc967933f6b8daba81de7f8dcbc6a
Deepak Dubey <deepakspc@Deepaks-MacBook-Air-3.local>
2026-02-17 11:14:22 +0530

    Fix error handling in append.js for missing file and update to use fs.unlink callback.

--- NodeJS/append.js
+++ NodeJS/append.js
@@ -5,7 +5,18 @@ const fs = require('fs');
     //console.log('Data Appended Successfully');
 //});
 //fs.copyFileSync('./.abes.txt', './a1.txt');
-fs.copyFile('./.abes.txt', './a2.txt', e => {
-    if (e) throw e;
-    console.log('File Copied Successfully');
+//fs.copyFile('./.abes.txt', './a2.txt', e => {
+    //if (e) throw e;
+    //console.log('File Copied Successfully');
+//});
+//fs.unlinkSync("./f1.txt");
+fs.unlink("./f2.txt", e => {
+    if (e) {
+        if (e.code === 'ENOENT') {
+            console.log('File not found, nothing to delete');
+            return;
+        }
+        throw e;
+    }
+    console.log('File Deleted Successfully');
 });
